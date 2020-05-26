adca46cd0e81c3fed8d2f119fd7d3f5b4dc73384
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-05-26 22:53:18 +0530

    wip: remove devel

--- install-preq.sh
+++ install-preq.sh
@@ -3,7 +3,7 @@ set -ex
 
 yum update -y
 yum install -y centos-release-scl scl-utils epel-release
-yum install -y python-pip python-devel 
+yum install -y python-pip
 yum install -y devtoolset-8
 pip install --upgrade pip
 pip install virtualenv
