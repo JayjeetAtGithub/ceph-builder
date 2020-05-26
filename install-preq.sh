7f9b7f478c3eb3bd4c4f64e95524e69609a9edb7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-05-26 22:28:01 +0530

    wip: install epel-release and python-devel also

--- install-preq.sh
+++ install-preq.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 
-yum install -y centos-release-scl scl-utils devtoolset-8 python-pip
+yum update -y
+yum install -y centos-release-scl scl-utils devtoolset-8 epel-release
+yum install -y python-pip python-devel
 pip install --upgrade pip
 pip install virtualenv
 scl enable devtoolset-8 bash
