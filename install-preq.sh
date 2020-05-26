2afe5178890a3b4632d84b667d99594892c76fd7
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-05-27 00:02:46 +0530

    add --upgrade flag to venv

--- install-preq.sh
+++ install-preq.sh
@@ -6,5 +6,5 @@ yum install -y centos-release-scl scl-utils epel-release
 yum install -y python-pip
 yum install -y devtoolset-8
 pip install --upgrade pip
-pip install virtualenv
+pip install --upgrade virtualenv
 scl enable devtoolset-8 bash
