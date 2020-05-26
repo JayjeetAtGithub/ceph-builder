5525fda62e037200f3f899c87e0dc3a282a1536c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-05-27 01:14:06 +0530

    comment out installations

--- install-preq.sh
+++ install-preq.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
 
-yum update -y
+# yum update -y
 yum install -y centos-release-scl scl-utils epel-release
 yum install -y python-pip
 yum install -y devtoolset-8
-pip install --upgrade pip
-pip install --upgrade virtualenv
+# pip install --upgrade pip
+# pip install --upgrade virtualenv
 scl enable devtoolset-8 bash
