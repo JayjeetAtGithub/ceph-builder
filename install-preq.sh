dc7a694598a4a3930b5c4dc9dcd1a8439c499a03
Jayjeet Chakraborty <jc.github@rediffmail.com>
2020-05-27 21:14:20 +0530

    install pip and virtualenv also

--- install-preq.sh
+++ install-preq.sh
@@ -5,7 +5,7 @@ set -ex
 yum install -y centos-release-scl scl-utils epel-release git wget gnupg2 ccache
 yum install -y python-pip
 yum install -y devtoolset-8 devtoolset-7
-# pip install --upgrade pip
-# pip install --upgrade virtualenv
+pip install --upgrade pip
+pip install --upgrade virtualenv
 scl enable devtoolset-8 bash
 scl enable devtoolset-7 bash
